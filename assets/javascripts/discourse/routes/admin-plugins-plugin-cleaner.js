9f0970abe4f32b456ff97f21264d1ece7df28138
canbekcan <can@canbekcan.com>
2026-03-28 23:42:10 +0200

    remove

--- assets/javascripts/discourse/routes/admin-plugins-plugin-cleaner.js
+++
@@ -1,7 +0,0 @@
-import DiscourseRoute from "discourse/routes/discourse";
-
-export default class AdminPluginsPluginCleanerRoute extends DiscourseRoute {
-  model() {
-    return {};
-  }
-}
--- lib/plugin_cleaner/version_logger.rb
+++
@@ -1,9 +0,0 @@
-module PluginCleaner
-  class VersionLogger
-    def self.snapshot!
-      PluginCleaner::VersionLog.snapshot_current_plugins!
-    rescue => e
-      Rails.logger.warn "[PluginCleaner] VersionLogger error: #{e.message}"
-    end
-  end
-end
